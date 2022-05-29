0a379d42dd0eba4d9805ecb0819c10148691b739
John Kourtesis <jkourtesis7@gmail.com>
2022-05-29 12:21:25 -0400

    Correct spelling mistake

--- sinsemilla_journey/views.py
+++ sinsemilla_journey/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('''Sinsemilla Journey: A story of
-        frustrasted ladies and their caretaker''')
+        frustrated ladies and their caretaker''')

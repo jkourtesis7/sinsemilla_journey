111a1019ddd3f1393e6bb11cab624e67ccd41baa
John Kourtesis <jkourtesis7@gmail.com>
2022-05-29 13:42:11 -0400

    Pylint corrections

--- sinsemilla_journey/views.py
+++ sinsemilla_journey/views.py
@@ -1,5 +1,11 @@
+"""
+First view for Sinsemilla Journey
+"""
 from django.http import HttpResponse
 
 def index(request):
+    """
+    Index request
+    """
     return HttpResponse('''Sinsemilla Journey: A story of
         frustrated ladies and their caretaker''')
